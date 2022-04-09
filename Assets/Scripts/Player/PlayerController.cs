cbf77a62242876b15b27945bb2124c137ce5f0aa
Kevin Wyss <wyss.kevin@outlook.com>
2022-04-09 18:21:55 +0200

    Loading next Scene
    Add the SceneManager on AreaExit Script to Load an another Scene

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -17,7 +17,7 @@ namespace Player
         // Start is called before the first frame update
         void Start()
         {
-
+            DontDestroyOnLoad(this);
         }
 
         // Update is called once per frame

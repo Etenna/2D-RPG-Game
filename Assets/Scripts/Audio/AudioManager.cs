864cf9ac8f3e1223ff6104077d6df3d971234034
Kevin Wyss <wyss.kevin@outlook.com>
2022-04-09 21:04:35 +0200

    Created a temporarily AudioManager for some Music vibes

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance!=null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

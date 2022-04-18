1109f28ee7be84220ed1a52caef947bbbc566223
Kevin Wyss <wyss.kevin@outlook.com>
2022-04-18 18:44:54 +0200

    Add Calculate XP for Each Level from DarkSouls

--- Assets/Scripts/Characters/PlayerStats.cs
+++ Assets/Scripts/Characters/PlayerStats.cs
@@ -6,8 +6,12 @@ public class PlayerStats : MonoBehaviour
 {
     [Header("Character Info")]
     [SerializeField] string playerName;
+    [Header("Level Info")]
     [SerializeField] int playerLevel=1;
     [SerializeField] int currentXP;
+    [SerializeField] int maxLevel=10;
+    [SerializeField] int baseLevelXP = 100; // XP need for reach the second Level
+    [SerializeField] int[] xpForEachLevel;
 
     [Header("Health and Mana Info")]
     [SerializeField] int maxHealth = 100;
@@ -24,10 +28,25 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] int defence;
     [SerializeField] int luck;
 
+    [Header("XP Calculator")]
+    [SerializeField] float xpCalc1 = 5.12f;
+    [SerializeField] float xpCalc2 = 4.06f;
+    [SerializeField] float xpCalc3 = 110.6f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        xpForEachLevel = new int[maxLevel];
+        xpForEachLevel[1] = baseLevelXP;
+        CalculateXPForEachLevel();
+    }
+
+    private void CalculateXPForEachLevel()
+    {
+        for (int i = 2; i < xpForEachLevel.Length; i++)
+        {
+            xpForEachLevel[i] = Mathf.FloorToInt(xpCalc1 * i * i * i + xpCalc2 * i * i + xpCalc3 * i);
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/Dialog/DialogHandler.cs
+++ Assets/Scripts/Dialog/DialogHandler.cs
@@ -5,40 +5,40 @@ using UnityEngine;
 
 public class DialogHandler : MonoBehaviour
 {
-    public static DialogHandler instance;
+    //public static DialogHandler instance;
 
-    [SerializeField] string villagerName;
-    [SerializeField] int villagerID=0;
+    //[SerializeField] string villagerName;
+    //[SerializeField] int villagerID=0;
 
-    public string[] sentences;
-    bool canActivateBox;
-    // Start is called before the first frame update
-    void Start()
-    {
-        instance = this;
-    }
+    //public string[] sentences;
+    //bool canActivateBox;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    instance = this;
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (canActivateBox && Input.GetKeyDown(KeyCode.Joystick1Button0))
-        {
-            //EventManager.OnConversationStartEvent(sentences,villagerID,villagerName);
-        }
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (canActivateBox && Input.GetKeyDown(KeyCode.Joystick1Button0))
+    //    {
+    //        //EventManager.OnConversationStartEvent(sentences,villagerID,villagerName);
+    //    }
+    //}
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            canActivateBox = true;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            canActivateBox = false;
-        }
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        canActivateBox = true;
+    //    }
+    //}
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        canActivateBox = false;
+    //    }
+    //}
 }

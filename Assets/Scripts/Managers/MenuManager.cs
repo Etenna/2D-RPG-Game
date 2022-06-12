ef24f2e4c81d2239450d90c33b04d7a557d96d35
Kevin Wyss <wyss.kevin@outlook.com>
2022-06-12 11:00:12 +0200

    Implemented the MenuManager
    If the Player press a certain Key, the Menu will appear

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager instance;
+    [SerializeField] GameObject menu;
+
+    bool menuActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this);
+        menu.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button7))
+        {
+            ShowMenu();
+        }
+    }
+
+    private void ShowMenu()
+    {
+        if (menu.activeInHierarchy)
+        {
+            menu.gameObject.SetActive(false);
+        }
+        else
+        {
+            menu.gameObject.SetActive(true);
+        }
+    }
+}
